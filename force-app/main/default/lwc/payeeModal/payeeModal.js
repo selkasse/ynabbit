3ab48631f1465977e85b5825819399a2f6fef3de
Sharif Elkassed <selkasse@gmu.edu>
2023-07-04 12:22:23 -0400

    add mock payee modal

--- force-app/main/default/lwc/payeeModal/payeeModal.js
+++ force-app/main/default/lwc/payeeModal/payeeModal.js
@@ -1,6 +1,24 @@
 import LightningModal from "lightning/modal";
+import { api } from "lwc";
 
+// const columns = [{ label: "Payee Name", fieldName: "Name" }];
 export default class PayeeModal extends LightningModal {
+  @api content;
+  //   columns = columns;
+
+  value = "";
+
+  get options() {
+    let opts = [];
+    this.content.forEach((payee) => {
+      let option = {};
+      option.label = payee.Name;
+      option.value = payee.Id;
+      opts.push(option);
+    });
+    return opts;
+  }
+
   handleOkay() {
     this.close(`okay`);
   }
--- force-app/main/default/lwc/preRecordedTransactionEntry/preRecordedTransactionEntry.js
+++ force-app/main/default/lwc/preRecordedTransactionEntry/preRecordedTransactionEntry.js
@@ -1,11 +1,21 @@
 import { LightningElement } from "lwc";
+import getPayees from "@salesforce/apex/PreRecordedTransactionController.getPayees";
 import payeeModal from "c/payeeModal";
 
 export default class PreRecordedTransactionEntry extends LightningElement {
-  size = "large";
+  size = "small";
+  payees;
+
+  async connectedCallback() {
+    const payeesResult = await getPayees();
+    this.payees = payeesResult;
+    console.log(`this.payees:`);
+    console.log(JSON.stringify(this.payees));
+  }
   async handlePayeeClick() {
     const payeeClickResult = await payeeModal.open({
-      size: this.size
+      size: this.size,
+      content: this.payees
     });
     console.log(payeeClickResult);
   }
