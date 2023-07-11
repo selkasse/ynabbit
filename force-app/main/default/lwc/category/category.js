8d7fb595206c81bfbaa1de675f2f3ccdaca26b4c
Sharif Elkassed <selkasse@gmu.edu>
2023-07-11 07:02:20 -0400

    category selection on mobile WIP

--- force-app/main/default/lwc/category/category.js
+++ force-app/main/default/lwc/category/category.js
@@ -1,8 +1,22 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement, api, wire } from "lwc";
+
+import { publish, MessageContext } from "lightning/messageService";
+import YNABBITMessageChannel from "@salesforce/messageChannel/YNABBITMessageChannel__c";
 
 const columns = [
-  { label: "Name", fieldName: "Name" },
-  { label: "Amount", fieldName: "Available__c", type: "currency" }
+  {
+    label: "Name",
+    fieldName: "Name",
+    // fixedWidth: 550,
+    cellAttributes: { class: "slds-theme_shade" }
+  },
+  {
+    label: "Amount",
+    fieldName: "Available__c",
+    type: "currency",
+    // fixedWidth: 80,
+    cellAttributes: { class: "slds-theme_shade" }
+  }
 ];
 
 export default class Category extends LightningElement {
@@ -12,12 +26,13 @@ export default class Category extends LightningElement {
   data;
   hasData = false;
 
+  @wire(MessageContext)
+  messageContext;
+
   connectedCallback() {
-    console.log(`this.category: ${JSON.stringify(this.category)}`);
     let data = [];
     data.push(this.category);
     this.data = data;
-    console.log(`this.data: ${this.data}`);
   }
 
   renderedCallback() {
@@ -27,5 +42,13 @@ export default class Category extends LightningElement {
     console.log(
       `you clicked ${this.data[0].Name} with Id of ${this.data[0].Id}`
     );
+    const payload = {
+      categoryId: this.data[0].Id,
+      categoryName: this.data[0].Name
+    };
+    publish(this.messageContext, YNABBITMessageChannel, payload);
+
+    //   TODO: need to listen for this Lightning Message Service event in the modal and close there
+    // this.close(`okay`);
   }
 }
--- force-app/main/default/lwc/categoryModal/categoryModal.js
+++ force-app/main/default/lwc/categoryModal/categoryModal.js
@@ -1,15 +1,9 @@
 import LightningModal from "lightning/modal";
-import { api, wire } from "lwc";
-
-import { publish, MessageContext } from "lightning/messageService";
-import YNABBITMessageChannel from "@salesforce/messageChannel/YNABBITMessageChannel__c";
+import { api } from "lwc";
 
 export default class CategoryModal extends LightningModal {
   @api content;
   isSplit = false;
-
-  @wire(MessageContext)
-  messageContext;
 
   handleSplit() {
     this.isSplit = true;
